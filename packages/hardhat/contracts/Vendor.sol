b94c459f73e3f08c958426ae1205b9c5493b2f76
tanhengyeow <25320782+tanhengyeow@users.noreply.github.com>
2023-09-29 19:29:15 +0800

    feat: complete up to checkpoint 2 + side quest

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,32 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+	// State variables
+	YourToken public yourToken;
+	uint256 public constant tokensPerEth = 100;
 
-  YourToken public yourToken;
+	// Events
+	event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+	constructor(address tokenAddress) {
+		yourToken = YourToken(tokenAddress);
+	}
 
-  // ToDo: create a payable buyTokens() function:
+	function buyTokens() public payable {
+		// Amount of tokens represented in 1e18
+		uint256 amountOfTokens = tokensPerEth * msg.value;
+		yourToken.transfer(msg.sender, amountOfTokens);
+		emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+	}
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+	function withdrawTokens() public onlyOwner {
+		payable(msg.sender).transfer(address(this).balance);
+	}
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
+	// ToDo: create a sellTokens(uint256 _amount) function:
 }
