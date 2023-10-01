4399373c2d6938a57d010b7c37daf2be2268d1b8
tanhengyeow <25320782+tanhengyeow@users.noreply.github.com>
2023-10-01 18:27:43 +0800

    feat: complete up to checkpoint 2 (reserves)

--- packages/hardhat/contracts/Balloons.sol
+++ packages/hardhat/contracts/Balloons.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Balloons is ERC20 {
-    constructor() ERC20("Balloons", "BAL") {
-        _mint(msg.sender, 1000 ether); // mints 1000 balloons!
-    }
+	constructor() ERC20("Balloons", "BAL") {
+		_mint(msg.sender, 1000 ether); // mints 1000 balloons!
+	}
 }
--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -46,25 +46,23 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
 
   const dex = await hre.ethers.getContract("DEX", deployer);
 
-    // paste in your front-end address here to get 10 balloons on deploy:
-    // await balloons.transfer(
-    //     "YOUR_FRONTEND_ADDRESS",
-    //     "" + 10 * 10 ** 18
-    // );
+  // paste in your front-end address here to get 10 balloons on deploy:
+  await balloons.transfer(
+      "0x902fF9039fd8524d638ef55Be0102e5a6f47Ba71",
+      "" + 10 * 10 ** 18
+  );
 
-
-    // uncomment to init DEX on deploy:
-
-    // console.log(
-    //   "Approving DEX (" + dex.address + ") to take Balloons from main account..."
-    // );
-    // // If you are going to the testnet make sure your deployer account has enough ETH
-    // await balloons.approve(dex.address, hre.ethers.utils.parseEther("100"));
-    // console.log("INIT exchange...");
-    // await dex.init(hre.ethers.utils.parseEther("5"), {
-    //   value: hre.ethers.utils.parseEther("5"),
-    //   gasLimit: 200000,
-    // });
+  // uncomment to init DEX on deploy:
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address, hre.ethers.utils.parseEther("100"));
+  console.log("INIT exchange...");
+  await dex.init(hre.ethers.utils.parseEther("5"), {
+    value: hre.ethers.utils.parseEther("5"),
+    gasLimit: 200000,
+  });
 
 };
 
